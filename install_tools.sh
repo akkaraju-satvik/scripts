106e5ff3dd08525853def661490de809cad86916
Satvik Akkaraju <satvik.akkaraju@gmail.com>
2024-01-28 23:43:03 +0530

    synced changes manually at 2024-01-28_23:43

--- install_tools.sh
+++ install_tools.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
+nvm use node
+
+# NODE
+sudo apt install npm
+npm install -g tsc @angular/cli nodemon dotenv
 
 # GOLANG
-curl https://dl.google.com/go/go1.21.6.linux-amd64.tar.gz -o go.tar.gz
+curl https://dl.google.com/go/go1.21.6.linux-amd64.tar.gz -o ~/go.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf ~/go.tar.gz
 export PATH=$PATH:/usr/local/go/bin

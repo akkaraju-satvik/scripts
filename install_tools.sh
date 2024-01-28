c30d8830d939f4523fef98953d2f3dde71fb2905
Satvik Akkaraju <satvik.akkaraju@gmail.com>
2024-01-28 23:21:32 +0530

    synced changes manually at 2024-01-28_23:21

--- install_tools.sh
+++ install_tools.sh
@@ -2,8 +2,14 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+# GOLANG
 curl https://dl.google.com/go/go1.21.6.linux-amd64.tar.gz -o go.tar.gz
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go.tar.gz
+sudo tar -C /usr/local -xzf ~/go.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
+
+# RUST
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+
--- sync.sh
+++ sync.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 gitCommitAndPush() {
+  mode=""
+  if [[ $2 ]]; then
+    mode="automatically"
+  else
+    mode="manually"
+  fi
   if [[ `git status --porcelain` ]]; then
     git add .
-    git commit -m "synced changes at $(date +%F_%R)"
+    git commit -m "synced changes $mode at $(date +%F_%R)"
     git push
   else
     echo "No changes to sync in $1"
@@ -12,10 +18,10 @@ gitCommitAndPush() {
 echo "SYNCING NOTES..."
 cd ~/notes
 
-gitCommitAndPush notes
+gitCommitAndPush notes $1
 
 echo "SYNCING SCRIPTS..."
 cd ~/scripts
 
-gitCommitAndPush scripts
+gitCommitAndPush scripts $1
 

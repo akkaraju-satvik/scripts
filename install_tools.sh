e0170dc6fc1ded2b52a6e0ad9787ea8da50b6feb
Satvik Akkaraju <satvik.akkaraju@gmail.com>
2024-01-28 23:09:09 +0530

    synced changes at 2024-01-28_23:09

--- install_tools.sh
+++ install_tools.sh
@@ -2,8 +2,8 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-sudo apt-get purge golang*
 curl https://dl.google.com/go/go1.21.6.linux-amd64.tar.gz -o go.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version

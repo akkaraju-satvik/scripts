bae79a0e5b8a3859688d8285c2429ec93c1dbe7d
Satvik Akkaraju <satvik.akkaraju@gmail.com>
2024-02-01 11:00:01 +0530

    synced changes automatically at 2024-02-01_11:00

--- stop-all-containers.sh
+++ stop-all-containers.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 if [[ $(docker ps --format '{{.Names}}') ]]; then
+  echo "Stopping containers..."
   docker stop $(docker ps --format '{{.Names}}')
 else
-  echo "No containers running at the moment!"
+  if [[ ! $1 ]]; then
+    echo "No containers running at the moment!"
+  fi
 fi
 
 if [[ $1 ]]; then
   case $1 in
     -r)
+      echo "Removing containers..."
       docker rm $(docker ps -a --format '{{.Names}}')
       ;;
   esac
